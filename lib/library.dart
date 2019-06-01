64d2f45aba1d7790c3f0a5b046669a2dffdbcdc8
GroovinChip <groovinchip@gmail.com>
2019-06-01 11:52:20 -0400

    fix path issues

--- lib/library.dart
+++ lib/library.dart
@@ -7,4 +7,8 @@ export 'package:flutter/foundation.dart';
 export 'package:cloud_firestore/cloud_firestore.dart';
 
 /// Screens
-export 'package:flubber_nyc_hack19/screens/home_screen/home_screen/home_screen.dart';
+export 'package:flubber_nyc_hack19/screens/home_screen/home_screen.dart';
+export 'package:flubber_nyc_hack19/screens/challenge_suggestions/challenge_suggestions.dart';
+export 'package:flubber_nyc_hack19/screens/current_challenge/current_challenge.dart';
+export 'package:flubber_nyc_hack19/screens/hall_of_fame/hall_of_fame.dart';
+export 'package:flubber_nyc_hack19/screens/upcoming_challenges/upcoming_challenges.dart';
