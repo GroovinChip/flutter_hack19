9bfc28fcc94a0aa530ac7b4ab930bdde17085ad5
GroovinChip <groovinchip@gmail.com>
2019-06-01 11:00:30 -0400

    initial

---
+++ lib/library.dart
@@ -0,0 +1,9 @@
+/// Packages
+export 'package:flutter/material.dart';
+export 'package:flutter/services.dart';
+export 'dart:io' show Platform;
+export 'package:flutter/foundation.dart' show debugDefaultTargetPlatformOverride;
+export 'package:flutter/foundation.dart';
+
+/// Screens
+export 'screens/home_screen/home_screen.dart';
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'library.dart';
+
+void main() {
+  _setTargetPlatformForDesktop();
+  runApp(MyApp());
+}
+
+/// If the current platform is desktop, override the default platform to
+/// a supported platform (iOS for macOS, Android for Linux and Windows).
+/// Otherwise, do nothing.
+void _setTargetPlatformForDesktop() {
+  TargetPlatform targetPlatform;
+  if (Platform.isMacOS) {
+    targetPlatform = TargetPlatform.iOS;
+  } else if (Platform.isLinux || Platform.isWindows) {
+    targetPlatform = TargetPlatform.android;
+  }
+  if (targetPlatform != null) {
+    debugDefaultTargetPlatformOverride = targetPlatform;
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flubber Community Challenges',
+      theme: ThemeData(
+        primaryColor: Colors.indigo,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
